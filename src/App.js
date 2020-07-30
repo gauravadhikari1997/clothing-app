e73f3e1f5e39a37131e9b4e4e7f056b371fafe52
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-07-30 15:34:07 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,17 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component.jsx";
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage}></Route>
-      <Route exact path="/shop" component={ShopPage}></Route>
-    </Switch>
+    <>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage}></Route>
+        <Route exact path="/shop" component={ShopPage}></Route>
+      </Switch>
+    </>
   );
 }
 
