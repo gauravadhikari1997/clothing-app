b76b9ec7ad941940a3e7ac057d5459e332fd242a
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-08-03 18:42:05 +0530

    removed redux-logger from build environment

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -74,7 +74,11 @@ const persistConfig = {
 };
 const pReducer = persistReducer(persistConfig, reducer);
 
-const middlewares = [logger];
+const middlewares = [];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push("logger");
+}
 
 const store = createStore(pReducer, applyMiddleware(...middlewares));
 const persistor = persistStore(store);
