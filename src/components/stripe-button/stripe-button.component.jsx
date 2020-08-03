1dbb6f5ae23e3a1f9a2364c59fe984bfcc81cd43
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-08-03 18:31:40 +0530

    integrated stripe and updated favicon and title of app

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = "pk_test_HS0jHHZbExVCpfVHeGM8yjgm004eKCRVhQ";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Succesful!");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="Classy Clothing Ltd."
+      billingAddress
+      shippingAddress
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
