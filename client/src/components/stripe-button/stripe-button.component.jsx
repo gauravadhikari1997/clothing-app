3cda352e9fb380e73cbc7558b3b2948de62b1785
Gaurav Adhikari <gaurav.15bme2023@abes.ac.in>
2020-08-05 12:48:03 +0530

    moved front end into /client and setup server for stripe

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = "pk_test_HS0jHHZbExVCpfVHeGM8yjgm004eKCRVhQ";
 
-  const onToken = (token) => {
-    console.log(token);
+  const onToken = async (token) => {
+    try {
+      await axios.post("/payment", { amount: priceForStripe, token });
+      alert("Payment Succesful!");
+    } catch (e) {
+      console.log("Payment error", JSON.parse(e));
+      alert("Some error occured! Please use the given credit card to test");
+    } finally {
+    }
     alert("Payment Succesful!");
   };
 
